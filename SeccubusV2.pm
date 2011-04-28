038528466ba24eefdba2fa65ce97a689c4ebaa13
Frank Breedijk <frank@breedijk.net>
2011-04-28 16:46:09 +0000

    Trunk version is now alpha3

--- SeccubusV2.pm
+++ SeccubusV2.pm
@@ -47,7 +47,7 @@ $config = "/opt/Seccubus/etc/config.xml" unless -e $config;
 use lib "/opt/Seccubus/SeccubusV2";
 push (@main::INC, @INC);
 
-$VERSION = '2.0.alpha2';
+$VERSION = '2.0.alpha3';
 
 use strict;
 use Carp;
