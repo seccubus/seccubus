48ab64584c835ff0f9cffed106828a792fc321b0
Frank Breedijk <frank@breedijk.net>
2011-11-21 12:43:35 +0000

    Messing with stuff to get it going again

--- jmvc/seccubus/seccubus.js
+++ jmvc/seccubus/seccubus.js
@@ -1,21 +1,23 @@
 steal(
-	
-	'./seccubus.css',		// application CSS file
-	'./models/models.js',		// steals all your models
-	//'./fixtures/fixtures.js',	// sets up fixtures for your models
-	'seccubus/tabs',		// get Seccubus tabs
+	'./seccubus.css', 			// application CSS file
+	'./models/models.js',			// steals all your models
+	'./fixtures/fixtures.js',		// sets up fixtures for your models
+	'seccubus/tabs',
 	'seccubus/up_to_date/list',
 	'seccubus/config_item/list',
-	function(){			// configure your application
-		
-		
-       $('#navTab').seccubus_tabs();
-       // Disable the Scans and Findings tab on start
-       $('#navTab').seccubus_tabs("disable", 2);
-       $('#navTab').seccubus_tabs("disable", 3);
-       // Hide Issues and Reports tab for now
-       $('#navTab').seccubus_tabs("hide", 4);
-       $('#navTab').seccubus_tabs("hide", 5);
-       $('#up_to_dates').seccubus_up_to_date_list();
-       $('#config_items').seccubus_config_item_list();
-})
+	function(){					// configure your application
+		$('#navTab').seccubus_tabs();
+		// Disable the Scans and Findings tab on start
+		$('#navTab').seccubus_tabs("disable", 2);
+		$('#navTab').seccubus_tabs("disable", 3);
+		// Hide Issues and Reports tab for now
+		$('#navTab').seccubus_tabs("hide", 4);
+		$('#navTab').seccubus_tabs("hide", 5);
+
+		// UpToDate status
+		$('#up_to_dates').seccubus_up_to_date_list();
+
+		// ConfigItem status
+		$('#config_items').seccubus_config_item_list();
+}
+)
