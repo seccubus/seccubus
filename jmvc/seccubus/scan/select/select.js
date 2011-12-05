e1c41d17695568ea9c8347af7217beeab77c0205
Frank Breedijk <frank@breedijk.net>
2011-12-05 17:17:59 +0000

    Select element for scans

---
+++ jmvc/seccubus/scan/select/select.js
@@ -0,0 +1,42 @@
+steal( 'jquery/controller',
+	   'jquery/view/ejs',
+	   'jquery/controller/view',
+	   'seccubus/models' )
+.then( './views/init.ejs', 
+       './views/scan.ejs', 
+       function($){
+
+/**
+ * @class Seccubus.Scan.Select
+ * @parent index
+ * @inherits jQuery.Controller
+ * Selects scans 
+ */
+$.Controller('Seccubus.Scan.Select',
+/** @Static */
+{
+	defaults : {}
+},
+/** @Prototype */
+{
+	init : function(){
+		this.element.html(this.view('init',Seccubus.Models.Scan.findAll()) )
+	},
+	'.destroy click': function( el ){
+		if(confirm("Are you sure you want to destroy?")){
+			el.closest('.scan').model().destroy();
+		}
+	},
+	"{Seccubus.Models.Scan} destroyed" : function(Scan, ev, scan) {
+		scan.elements(this.element).remove();
+	},
+	"{Seccubus.Models.Scan} created" : function(Scan, ev, scan){
+		this.element.append(this.view('init', [scan]))
+	},
+	"{Seccubus.Models.Scan} updated" : function(Scan, ev, scan){
+		scan.elements(this.element)
+		      .html(this.view('scan', scan) );
+	}
+});
+
+});
