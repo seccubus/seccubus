702f541bc8cab8134d78ff3adbadfde9c509be15
Frank Breedijk <frank@breedijk.net>
2011-12-28 16:08:06 +0000

    Basic findings table integrated into seccubus display

--- jmvc/seccubus/seccubus.js
+++ jmvc/seccubus/seccubus.js
@@ -7,8 +7,9 @@ steal(
 	'seccubus/up_to_date/list',
 	'seccubus/config_item/list',
 	'seccubus/workspace/select',
-	'seccubus/workspace/table',
 	'seccubus/scan/select',
+	'seccubus/finding/table',
+	//'seccubus/workspace/table',
 	function(){					// configure your application
 		/***********************************************************
 		 * Initialize gui state and hook into it
@@ -18,15 +19,18 @@ steal(
 			workspace	: -1
 		});
 		gui_state.bind("workspace", function(ev, ws){
-			$('select.scanSelector').each( function() {
-				$(this).seccubus_scan_select({workspace : ws});
-			});
+			render_scan_selectors();
+			render_finding_table();
+		});
+		gui_state.bind("scans", function(ev, scan){
+			render_finding_table();
 		});
 
 		/***********************************************************
 		 * Setup the screen
 		 **********************************************************/
 
+		// Tabs
 		$('#navTab').seccubus_tabs();
 		// Disable the Scans tab on start
 		$('#navTab').seccubus_tabs("disable", 3);
@@ -48,22 +52,44 @@ steal(
 			// Trap change events
 			$(this).change(function() {
 				newWorkspace = $(this).attr("value");
+				// Update the gui state
 				gui_state.attr("workspace",newWorkspace);
+				// Set all other selectors
 				$('.workspaceSelector').attr("value",newWorkspace);
 
 			});
 		});
-		//$('#workspace_selector_scans').seccubus_workspace_select();
-		//$('#workspace_selector_findings').seccubus_workspace_select();
-		//$('#workspace_selector_issues').seccubus_workspace_select();
 
-		// Apparently this needs to be called last
-		//$('#workspace_table').seccubus_workspace_table();
-		//$('#create').seccubus_workspace_create();
-		
 		// Setup all scan selectors
+		render_scan_selectors();
 		$('.scanSelector').each( function() {
-			$(this).seccubus_scan_select();
+			$(this).change(function() {
+				var val = $(this).val();
+				gui_state.attr("scans",val);
+				$('.scanSelector').val(val);
+			});
 		});
-}
+
+		// Setup finding table
+		render_finding_table();
+
+		/**********************************************************
+		 * Functions
+		 *********************************************************/
+
+		function render_scan_selectors() {
+			$('select.scanSelector').each( function() {
+				$(this).seccubus_scan_select({
+					workspace : gui_state.workspace
+				});
+			});
+		};
+
+		function render_finding_table() {
+			$('#finding_table').seccubus_finding_table({
+					workspace : gui_state.workspace,
+					scans     : gui_state.scans,
+			});
+		};
+	}
 )
