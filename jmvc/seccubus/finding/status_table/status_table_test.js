c93bcc745ef2fc60b8b6ab714953f80b9cff052b
Frank Breedijk <frank@breedijk.net>
2011-12-29 13:40:45 +0000

    Cleanup

--- jmvc/seccubus/finding/status_table/status_table_test.js
+++ jmvc/seccubus/finding/status_table/status_table_test.js
@@ -1,13 +1,13 @@
 steal('funcunit').then(function(){
 
-module("Seccubus.Finding.Table", { 
+module("Seccubus.Finding.StatusTable", { 
 	setup: function(){
 		S.open("//seccubus/finding/status_table/status_table.html");
 	}
 });
 
 test("Text Test", function(){
-	equals(S("h1").text(), "Seccubus.Finding.Table Demo","demo text");
+	equals(S("h1").text(), "Seccubus.Finding.StatusTable Demo","demo text");
 });
 
 
