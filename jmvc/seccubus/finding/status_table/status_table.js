fd11c1b081e1b4095ae058fe2e0c87b829a8e868
Frank Breedijk <frank@breedijk.net>
2011-12-29 10:27:05 +0000

    Status widget is working as well

---
+++ jmvc/seccubus/finding/status_table/status_table.js
@@ -0,0 +1,55 @@
+steal(	'jquery/controller',
+	'jquery/view/ejs',
+	'jquery/controller/view',
+	'seccubus/models' )
+.then(	'./views/init.ejs',
+	'./views/finding.ejs',
+	'./views/error.ejs',
+function($){
+
+/**
+ * @class Seccubus.Finding.StatusTable
+ */
+$.Controller('Seccubus.Finding.StatusTable',
+/** @Static */
+{
+	defaults : {
+		workspace	: -1,
+		scans		: null,
+	}
+},
+/** @Prototype */
+{
+	init : function(){
+		this.updateView();
+	},
+	'.setStatus clicked' : function(el) {
+		alert("Yes we did it");
+	},
+	updateView : function() {
+		if ( this.options.workspace < 0  ) {
+			this.element.html(
+				this.view('error', {message : "Please select a workspace to start"})
+			);
+		} else if ( this.options.scans == null ) {
+			this.element.html(
+				this.view('error', {message : "Please select one or more scans"})
+			);
+		} else {
+			this.element.html(
+				this.view(
+					'init',
+					Seccubus.Models.Finding.findAll(), {
+						fScans : this.options.scans,
+					}
+				)
+			);
+		}
+	},
+	update : function(options){
+		this._super(options);
+		this.updateView();
+	}
+}) // Controller
+
+}); // Steal
