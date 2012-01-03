ebc12b8c4126b4d1fe92bd453304527e6b0863a8
Frank Breedijk <frank@breedijk.net>
2012-01-03 10:57:10 +0000

    Stub

---
+++ www/json/getNothing.pl
@@ -0,0 +1,49 @@
+#!/usr/bin/env perl
+# ------------------------------------------------------------------------------
+# $Id$
+# ------------------------------------------------------------------------------
+# List the scans
+# ------------------------------------------------------------------------------
+# Copyright (C) 2010  Schuberg Philis, Frank Breedijk - Under GPLv3
+# ------------------------------------------------------------------------------
+
+use strict;
+use CGI;
+use JSON;
+use lib "..";
+use SeccubusV2;
+use SeccubusScans;
+
+my $query = CGI::new();
+my $json = JSON->new();
+
+print $query->header("application/json");
+
+my $workspace_id = $query->param("workspaceId");
+
+# Return an error if the required parameters were not passed 
+if (not (defined ($workspace_id))) {
+	bye("Parameter workspaceId is missing");
+} elsif ( $workspace_id + 0 ne $workspace_id ) {
+	bye("WorkspaceId is not numeric");
+};
+
+eval {
+	my @data;
+	my $scans = (); # Get somethin here
+
+	foreach my $row ( @$scans ) {
+		push (@data, {
+			'id'		=> $$row[0],
+		});
+	}
+	print $json->pretty->encode(\@data);
+} or do {
+	bye(join "\n", $@);
+};
+
+sub bye($) {
+	my $error=shift;
+	print $json->pretty->encode([{ error => $error }]);
+	exit;
+}
