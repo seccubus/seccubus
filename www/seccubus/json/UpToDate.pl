25214fd991d64f612670525e0e4dfaa7b61721e0
Frank Breedijk <frank@breedijk.net>
2012-03-13 08:16:58 +0000

    Moving API where it belongs

---
+++ www/seccubus/json/UpToDate.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/env perl
+# ------------------------------------------------------------------------------
+# $Id$
+# ------------------------------------------------------------------------------
+# List the scans
+# ------------------------------------------------------------------------------
+
+# Fixes Ticket [ 2981907 ] - Online up2date check
+use strict;
+use lib "..";
+use CGI;
+use CGI::Carp qw(fatalsToBrowser);
+use SeccubusV2;
+use LWP::Simple;
+use JSON;
+
+#my (
+   #);
+
+my $query = CGI::new();
+my $json = JSON->new();
+my $data = [];
+
+print $query->header("application/json");
+
+my $verdict = get("http://v2.seccubus.com/up2date.json.pl?version=$SeccubusV2::VERSION");
+if ( ! $verdict ) {
+	print $json->pretty->encode( [ {
+		'status'	=> "Error",
+		'message'	=> "Cannot check version online! Online version checks are disabled",
+		'link'		=> "",
+	} ]);
+} else { 
+	print $verdict;
+}
+exit;
---
+++ www/seccubus/json/createScan.pl
@@ -0,0 +1,64 @@
+#!/usr/bin/env perl
+# ------------------------------------------------------------------------------
+# $Id$
+# ------------------------------------------------------------------------------
+# Updates the findings passed by ID with the data passed
+# ------------------------------------------------------------------------------
+use strict;
+use CGI;
+use CGI::Carp qw(fatalsToBrowser);
+use JSON;
+use lib "..";
+use SeccubusV2;
+use SeccubusScans;
+
+my $query = CGI::new();
+my $json = JSON->new();
+
+print $query->header("application/json");
+
+my $workspace_id = $query->param("workspaceId");
+my $name = $query->param("name");
+my $scanner = $query->param("scanner");
+my $parameters = $query->param("parameters");
+my $targets = $query->param("targets");
+
+# Return an error if the required parameters were not passed 
+if (not (defined ($workspace_id))) {
+	bye("Parameter workspaceId is missing");
+};
+if (not (defined ($name))) {
+	bye("Parameter name is missing");
+};
+if (not (defined ($scanner))) {
+	bye("Parameter scanner is missing");
+};
+if (not (defined ($parameters))) {
+	bye("Parameter parameters is missing");
+};
+if (not (defined ($targets))) {
+	bye("Parameter targets is missing");
+};
+
+eval {
+	my @data = ();
+	my $newid = create_scan($workspace_id,$name,$scanner,$parameters,$targets);
+	push @data, {
+		id		=> $newid,
+		name		=> $name,
+		scanner		=> $scanner,
+		parameters	=> $parameters,
+		targets		=> $targets,
+		workspace	=> $workspace_id,
+	};
+	print $json->pretty->encode(\@data);
+} or do {
+	bye(join "\n", $@);
+};
+
+sub bye($) {
+	my $error=shift;
+	print $json->pretty->encode([{ error => $error }]);
+	exit;
+}
+
