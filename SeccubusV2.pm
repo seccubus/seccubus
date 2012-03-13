0d1570c4e2492216eadd29bdc2151a2b6b6be7ae
Frank Breedijk <frank@breedijk.net>
2012-03-13 14:20:27 +0000

    Trunk is now Seccubus-2.0.beta4

--- SeccubusV2.pm
+++ SeccubusV2.pm
@@ -35,7 +35,7 @@ $config = "/opt/Seccubus/etc/config.xml" unless -e $config;
 use lib "/opt/Seccubus/SeccubusV2";
 push (@main::INC, @INC);
 
-$VERSION = '2.0.beta3';
+$VERSION = '2.0.beta4';
 
 use strict;
 use Carp;
