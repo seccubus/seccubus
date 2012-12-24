61d75afa910f172571afcb688fd54dc4d38c7209
Frank Breedijk <frank@breedijk.net>
2012-12-24 17:07:50 +0100

    Created an event selector

---
+++ jmvc/seccubus/models/event.js
@@ -0,0 +1,21 @@
+steal('jquery/model', function(){
+
+/**
+ * @class Seccubus.Models.Event
+ * @parent index
+ * @inherits jQuery.Model
+ * Wraps backend event services.  
+ */
+$.Model('Seccubus.Models.Event',
+/* @Static */
+{
+	findAll: "POST json/getEvents.pl",
+  	//findOne : "/events/{id}.json", 
+  	//create : "/events.json",
+ 	//update : "/events/{id}.json",
+  	//destroy : "/events/{id}.json"
+},
+/* @Prototype */
+{});
+
+})
--- jmvc/seccubus/test/funcunit/funcunit.js
+++ jmvc/seccubus/test/funcunit/funcunit.js
@@ -1,2 +1,2 @@
-steal("funcunit", 'seccubus/up_to_date/list/list_test.js', 'seccubus/config_item/create/create_test.js', 'seccubus/config_item/list/list_test.js', 'seccubus/workspace/create/create_test.js', 'seccubus/workspace/list/list_test.js', 'seccubus/scan/create/create_test.js', 'seccubus/scan/list/list_test.js')
+steal("funcunit", 'seccubus/up_to_date/list/list_test.js', 'seccubus/config_item/create/create_test.js', 'seccubus/config_item/list/list_test.js', 'seccubus/workspace/create/create_test.js', 'seccubus/workspace/list/list_test.js', 'seccubus/scan/create/create_test.js', 'seccubus/scan/list/list_test.js', 'seccubus/event/create/create_test.js', 'seccubus/event/list/list_test.js')
  .then("./seccubus_test.js");
