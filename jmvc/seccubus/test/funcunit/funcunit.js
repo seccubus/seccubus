206fae9dea76336aab665c18aee2804f6d29d66b
Frank Breedijk <frank@breedijk.net>
2013-03-11 23:01:45 +0100

    Added headers to all files possible

--- jmvc/seccubus/test/funcunit/funcunit.js
+++ jmvc/seccubus/test/funcunit/funcunit.js
@@ -1,2 +1,17 @@
+/*
+ * Copyright 2013 Frank Breedijk
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 steal("funcunit", 'seccubus/up_to_date/list/list_test.js', 'seccubus/config_item/create/create_test.js', 'seccubus/config_item/list/list_test.js', 'seccubus/workspace/create/create_test.js', 'seccubus/workspace/list/list_test.js', 'seccubus/scan/create/create_test.js', 'seccubus/scan/list/list_test.js', 'seccubus/event/create/create_test.js', 'seccubus/event/list/list_test.js', 'seccubus/notification/create/create_test.js', 'seccubus/notification/list/list_test.js')
  .then("./seccubus_test.js");
