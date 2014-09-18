ee62d0f1b147fb83ee4a9685922e8e086e3a191f
Frank Breedijk <fbreedijk@schubergphilis.com>
2014-09-18 12:25:12 +0200

    Upped version number

--- SeccubusV2.pm
+++ SeccubusV2.pm
@@ -48,7 +48,7 @@ $config = "etc/dummy.config.xml" unless -e $config;
 use lib "/opt/seccubus/SeccubusV2";
 push (@main::INC, @INC);
 
-$VERSION = '2.9';
+$VERSION = '2.10';
 
 use strict;
 use Carp;
