3a7c70998002e355a8d227c7ffd61e06891cfbd6
Frank Breedijk <fbreedijk@schubergphilis.com>
2014-12-06 00:37:37 +0100

    Correct attribution

--- SeccubusV2/SeccubusScans.pm
+++ SeccubusV2/SeccubusScans.pm
@@ -1,5 +1,5 @@
 # ------------------------------------------------------------------------------
-# Copyright 2014 Frank Breedijk, Steve Launius, Artien Bel (Ar0xA)
+# Copyright 2014 Frank Breedijk, Steve Launius, Artien Bel (Ar0xA), Petr
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
