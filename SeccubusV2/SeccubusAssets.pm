fbc39a9d2f5338ed8e1ca5f536df64c48d906531
Frank Breedijk <fbreedijk@schubergphilis.com>
2014-12-06 00:37:37 +0100

    Correct attribution

--- SeccubusV2/SeccubusAssets.pm
+++ SeccubusV2/SeccubusAssets.pm
@@ -1,5 +1,5 @@
 # ------------------------------------------------------------------------------
-# Copyright 2014 Arkanoi, Frank Breedijk
+# Copyright 2014 Arkanoi, Frank Breedijk, Petr
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
