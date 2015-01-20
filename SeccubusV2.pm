f471815fa69e294b569a4f42ffb39c59683cfa31
blabla1337 <glenntencate@gmail.com>
2015-01-20 14:42:40 +0100

    Update SeccubusV2.pm

--- SeccubusV2.pm
+++ SeccubusV2.pm
@@ -1,5 +1,5 @@
 # ------------------------------------------------------------------------------
-# Copyright 2015 Frank Breedijk, blabla1337
+# Copyright 2015 Frank Breedijk, blabla1337, Glenn ten Cate
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -48,7 +48,7 @@ $config = "etc/dummy.config.xml" unless -e $config;
 use lib "/opt/seccubus/SeccubusV2";
 push (@main::INC, @INC);
 
-$VERSION = '2.12';
+$VERSION = '2.13';
 
 use strict;
 use Carp;
