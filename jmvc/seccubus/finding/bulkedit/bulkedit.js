5ef95266090748ece4d78f7f8d64834708d941d9
Frank Breedijk <fbreedijk@schubergphilis.com>
2015-08-28 16:21:29 +0200

    More debug output gone

--- jmvc/seccubus/finding/bulkedit/bulkedit.js
+++ jmvc/seccubus/finding/bulkedit/bulkedit.js
@@ -76,7 +76,6 @@ $.Controller('Seccubus.Finding.Bulkedit',
 	},
 	// Handles clicks on the save button
 	".bulkSetStatus click" : function(el, ev) {
-		console.log('Click' + el);
 		ev.preventDefault();
 		$(el).val("Saving...");
 		var newStatus = $(el).attr("newStatus");
