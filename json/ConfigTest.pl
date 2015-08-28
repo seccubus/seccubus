50fea8886e1b0a18d43a9e2300e5a82267c83876
Frank Breedijk <fbreedijk@schubergphilis.com>
2015-08-28 14:39:57 +0200

    Fix copyright

--- json/ConfigTest.pl
+++ json/ConfigTest.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/env perl
-# Copyright 2014 Frank Breedijk, Petr
+# Copyright 2015 Frank Breedijk, Petr, Andreas Thieneman
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
