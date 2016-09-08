5ac7bd817311c16b138e7a66ce01dad3a222c9c7
Frank Breedijk <fbreedijk@schubergphilis.com>
2016-09-08 15:45:29 +0200

    Fixes #364 - auto_gen column was missing from asset_host table

--- SeccubusV2.pm
+++ SeccubusV2.pm
@@ -48,7 +48,7 @@ $config = "etc/dummy.config.xml" unless -e $config;
 use lib "/opt/seccubus/SeccubusV2";
 push (@main::INC, @INC);
 
-$VERSION = '2.26';
+$VERSION = '2.27';
 
 use strict;
 use Carp;
