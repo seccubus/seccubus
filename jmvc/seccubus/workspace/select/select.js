a02078accbf19baf3f99d46164e8991cbb71eaf3
Frank Breedijk <fbreedijk@schubergphilis.com>
2017-05-09 14:30:07 +0200

    Improve Codacy quality

--- jmvc/seccubus/workspace/select/select.js
+++ jmvc/seccubus/workspace/select/select.js
@@ -13,60 +13,62 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-steal(	
-	'jquery/controller',
-	'jquery/view/ejs',
-	'jquery/controller/view',
-	'seccubus/models'
+steal(
+	"jquery/controller",
+	"jquery/view/ejs",
+	"jquery/controller/view",
+	"seccubus/models"
      )
-.then( './views/init.ejs', 
-       './views/workspace.ejs',
-       './views/no_workspace.ejs', 
-       function($){
+.then(
+    "./views/init.ejs",
+    "./views/workspace.ejs",
+    function($){
 
-/**
- * @class Seccubus.Workspace.Select
- * @parent Workspace
- * @inherits jQuery.Controller
- * Builds a dropdown selector of workspaces
- */
-$.Controller('Seccubus.Workspace.Select',
-/** @Static */
-{
-	defaults : {}
-},
-/** @Prototype */
-{
-	/* This funciton calls updateView to render the control
-	 */
-	init : function(el, fn){
-		this.updateView();
-	},
-	// (re-)Render on delete
-	"{Seccubus.Models.Workspace} destroyed" : function(Workspace, ev, workspace) {
-		this.updateView();
-	},
-	// (re-)Render on create
-	"{Seccubus.Models.Workspace} created" : function(Workspace, ev, workspace){
-		this.updateView();
-	},
-	// Apend on update
-	"{Seccubus.Models.Workspace} updated" : function(Workspace, ev, workspace){
-		workspace.elements(this.element)
-		      .html(this.view('workspace', workspace) );
-	},
-	/*
-	 * This fuction rerenders the entire control with data from findAll
-	 */
-	updateView : function() {
-		this.element.html(
-			this.view(
-				'init',
-				Seccubus.Models.Workspace.findAll()
-			) 
-		);
-	}
+        /**
+         * @class Seccubus.Workspace.Select
+         * @parent Workspace
+         * @inherits jQuery.Controller
+         * Builds a dropdown selector of workspaces
+         */
+        $.Controller(
+            'Seccubus.Workspace.Select',
+            /** @Static */
+            {
+            	defaults : {}
+            },
+            /** @Prototype */
+            {
+            	/* This funciton calls updateView to render the control
+            	 */
+            	init : function(el, fn){
+            		this.updateView();
+            	},
+            	// (re-)Render on delete
+            	"{Seccubus.Models.Workspace} destroyed" : function(Workspace, ev, workspace) {
+            		this.updateView();
+            	},
+            	// (re-)Render on create
+            	"{Seccubus.Models.Workspace} created" : function(Workspace, ev, workspace){
+            		this.updateView();
+            	},
+            	// Apend on update
+            	"{Seccubus.Models.Workspace} updated" : function(Workspace, ev, workspace){
+            		workspace.elements(this.element)
+            		      .html(this.view('workspace', workspace) );
+            	},
+            	/*
+            	 * This fuction rerenders the entire control with data from findAll
+            	 */
+            	updateView : function() {
+            		this.element.html(
+            			this.view(
+            				'init',
+            				Seccubus.Models.Workspace.findAll()
+            			)
+            		);
+            	}
 
-}); // Controller
-
-}); // Steal
+            }
+        ); // Controller
+    }
+); // Steal
