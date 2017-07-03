11a36d8603455bf3bc4ed6657aa30767f590cfa6
Frank Breedijk <fbreedijk@schubergphilis.com>
2017-07-03 15:08:22 +0200

    Tabs to spaces

--- jmvc/seccubus/finding/edit/edit.js
+++ jmvc/seccubus/finding/edit/edit.js
@@ -13,13 +13,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-steal(	'jquery/controller',
-	'jquery/view/ejs',
-	'jquery/controller/view',
-	'jquery/dom/form_params',
-	'seccubus/models',
-	'seccubus/issue/table',
-	'seccubus/history/table'
+steal(    'jquery/controller',
+    'jquery/view/ejs',
+    'jquery/controller/view',
+    'jquery/dom/form_params',
+    'seccubus/models',
+    'seccubus/issue/table',
+    'seccubus/history/table'
 ).then( './views/init.ejs',
 function($){
 
@@ -37,102 +37,102 @@ function($){
 $.Controller('Seccubus.Finding.Edit',
 /** @Static */
 {
-	/*
-	 * @attribute options
-	 * Object that contains the options
-	 */
-	defaults : {
-		/* @attribute options.findings
-		 * Array of findings to edit
-		 */
-		findings : [],
-		/* @attribute options.index
-		 * Index of which finding in the array to edit
-		 * Default: 0
-		 */
-		index : 0,
-		/* @attribute options.hostory
-		 * jQuery query that defines where the history will be displayed
-		 * Default : null
-		 * If this attribute is null there will be no history displayed
-		 */
-		history : null,
-		/* @attribute options.workspace
-		 * Id of the current workspace
-		 */
-		workspace : -1
+    /*
+     * @attribute options
+     * Object that contains the options
+     */
+    defaults : {
+        /* @attribute options.findings
+         * Array of findings to edit
+         */
+        findings : [],
+        /* @attribute options.index
+         * Index of which finding in the array to edit
+         * Default: 0
+         */
+        index : 0,
+        /* @attribute options.hostory
+         * jQuery query that defines where the history will be displayed
+         * Default : null
+         * If this attribute is null there will be no history displayed
+         */
+        history : null,
+        /* @attribute options.workspace
+         * Id of the current workspace
+         */
+        workspace : -1
 
-	}
+    }
 },
 /** @Prototype */
 {
-	init : function(){
-		this.updateView();
-	},
-	updateView : function() {
-		if ( this.options.findings.length == 0 ) {
-			alert("Seccubus.Findings.Edit : No findings to edit");
-		} else if ( this.options.workspace == -1 ) {
-			alert("Seccubus.Findings.Edit : Workspace not set");
-		} else {
-			this.element.html(
-				this.view(
-					'init',
-					this.options.findings[this.options.index],
-					{
-						index : this.options.index,
-						length: this.options.findings.length
-					}
-				)
-			);
-			$('#issue_table_findings').seccubus_issue_table({
-				workspace 	: this.options.workspace,
-				finding		: this.options.findings[this.options.index].id
-			});
-		}
-		if ( this.options.history != null ) {
-			/* Display the finding history */
-			$(this.options.history).seccubus_history_table({
-				workspace	: this.options.workspace,
-				findingId	: this.options.findings[this.options.index].id
-			});
+    init : function(){
+        this.updateView();
+    },
+    updateView : function() {
+        if ( this.options.findings.length == 0 ) {
+            alert("Seccubus.Findings.Edit : No findings to edit");
+        } else if ( this.options.workspace == -1 ) {
+            alert("Seccubus.Findings.Edit : Workspace not set");
+        } else {
+            this.element.html(
+                this.view(
+                    'init',
+                    this.options.findings[this.options.index],
+                    {
+                        index : this.options.index,
+                        length: this.options.findings.length
+                    }
+                )
+            );
+            $('#issue_table_findings').seccubus_issue_table({
+                workspace      : this.options.workspace,
+                finding        : this.options.findings[this.options.index].id
+            });
         }
-	},
-	".editSetStatus click" : function(el,ev) {
-		ev.preventDefault();
-		var newState = $(el).attr("newstatus");
-		var param = this.element.formParams();
-		var f = this.options.findings[this.options.index];
-		f.attr("status",newState);
-		f.attr("remark",param.remark);
-		f.attr("append",0);
-		f.attr("workspace",this.options.workspace);
-		f.save();
-	},
-	".move click" : function(el,ev) {
-		ev.preventDefault();
-		this.options.index = $(el).attr("index");
-		this.updateView();
-	},
-	"{Seccubus.Models.Finding} updated" : function(Finding, ev, finding) {
-		var n = 0;
-		while(n < this.options.findings.length && this.options.findings[n].id != finding.id) {
-			n++;
-		}
-		if(n < this.options.findings.length) {
-			this.options.findings[n] = finding;
-		}
-		if(n == this.options.index) {
-			this.updateView();
-		}
-	},
-	/*
-	 * Update, overloaded to reder the control after and update even
-	 */
-	update : function(options) {
-		this._super(options);
-		this.updateView();
-	}
+        if ( this.options.history != null ) {
+            /* Display the finding history */
+            $(this.options.history).seccubus_history_table({
+                workspace    : this.options.workspace,
+                findingId    : this.options.findings[this.options.index].id
+            });
+        }
+    },
+    ".editSetStatus click" : function(el,ev) {
+        ev.preventDefault();
+        var newState = $(el).attr("newstatus");
+        var param = this.element.formParams();
+        var f = this.options.findings[this.options.index];
+        f.attr("status",newState);
+        f.attr("remark",param.remark);
+        f.attr("append",0);
+        f.attr("workspace",this.options.workspace);
+        f.save();
+    },
+    ".move click" : function(el,ev) {
+        ev.preventDefault();
+        this.options.index = $(el).attr("index");
+        this.updateView();
+    },
+    "{Seccubus.Models.Finding} updated" : function(Finding, ev, finding) {
+        var n = 0;
+        while(n < this.options.findings.length && this.options.findings[n].id != finding.id) {
+            n++;
+        }
+        if(n < this.options.findings.length) {
+            this.options.findings[n] = finding;
+        }
+        if(n == this.options.index) {
+            this.updateView();
+        }
+    },
+    /*
+     * Update, overloaded to reder the control after and update even
+     */
+    update : function(options) {
+        this._super(options);
+        this.updateView();
+    }
 })
 
 });
--- jmvc/seccubus/history/table/table.js
+++ jmvc/seccubus/history/table/table.js
@@ -13,12 +13,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-steal( 	'jquery/controller',
-	'jquery/view/ejs',
-	'jquery/controller/view',
-	'seccubus/models'
+steal(     'jquery/controller',
+    'jquery/view/ejs',
+    'jquery/controller/view',
+    'seccubus/models'
 ).then( './views/init.ejs',
-	'./views/history.ejs',
+    './views/history.ejs',
 function($){
 
 /**
@@ -29,66 +29,66 @@ function($){
 $.Controller('Seccubus.History.Table',
 /** @Static */
 {
-	/*
-	 * @attribute options
-	 * Object that holds the options in its attributes
-	 */
-	defaults : {
+    /*
+     * @attribute options
+     * Object that holds the options in its attributes
+     */
+    defaults : {
                 /*
-		 * @attribute options.workspace
-		 * The currently selected workspace,
-		 * -1 = no workspace
-		 */
-                workspace       : -1,
-		/*
-		 * @attribute options.findingId
-		 * The currently selected finding,
-		 * -1 = no finding selected
-		 */
-		findingId	: -1,
-		/*
-		 * @attribute options.asHTML
-		 * Function to translate a finding to HTML
-		 */
-		asHTML 		: function(txt){
-			var out = $('<div/>').text(txt).html();
-			out = out.replace(/\n/g,"<br>");
-			return(out);
-		}
-	}
+         * @attribute options.workspace
+         * The currently selected workspace,
+         * -1 = no workspace
+         */
+        workspace   : -1,
+        /*
+         * @attribute options.findingId
+         * The currently selected finding,
+         * -1 = no finding selected
+         */
+        findingId   : -1,
+        /*
+         * @attribute options.asHTML
+         * Function to translate a finding to HTML
+         */
+        asHTML      : function(txt){
+            var out = $('<div/>').text(txt).html();
+            out = out.replace(/\n/g,"<br>");
+            return(out);
+        }
+    }
 },
 /** @Prototype */
 {
-	init : function(){
-		this.updateView();
-	},
-	updateView : function() {
-		if ( this.options.workspace == -1 ) {
-			alert("Seccubus.History.Table: woorkspace not set");
-		} else if ( this.options.findingId == -1 ) {
-			alert("Seccubus.History.Table: findingId not set");
-		} else {
-			this.element.html(
-				this.view(
-					'init',
-					Seccubus.Models.History.findAll({
-						workspace   : this.options.workspace,
-						id          : this.options.findingId
-					}),
-					{
-						asHTML	    : this.options.asHTML
-					}
-				)
-			);
-		}
-	},
-	/*
-	 * Update, overloaded to rerender the table after and udpate event
-	 */
-	update : function(options) {
-		this._super(options);
-		this.updateView();
-	}
+    init : function(){
+        this.updateView();
+    },
+    updateView : function() {
+        if ( this.options.workspace == -1 ) {
+            alert("Seccubus.History.Table: workspace not set");
+        } else if ( this.options.findingId == -1 ) {
+            alert("Seccubus.History.Table: findingId not set");
+        } else {
+            this.element.html(
+                this.view(
+                    'init',
+                    Seccubus.Models.History.findAll({
+                        workspace   : this.options.workspace,
+                        id          : this.options.findingId
+                    }),
+                    {
+                        asHTML      : this.options.asHTML
+                    }
+                )
+            );
+        }
+    },
+    /*
+     * Update, overloaded to rerender the table after and udpate event
+     */
+    update : function(options) {
+        this._super(options);
+        this.updateView();
+    }
 })
 
 });
