afdd79e89d1a6317f1adcba714307116b2621980
Frank Breedijk <fbreedijk@schubergphilis.com>
2017-10-02 21:36:23 +0200

    Fix license on .sh files in deb dir

--- deb/makedeb.sh
+++ deb/makedeb.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# ------------------------------------------------------------------------------
-# Copyright 2017 Arkanoi, Frank Breedijk, Petr
+# Copyright 2017 Frank Breedijk
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
