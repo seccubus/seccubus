3b563076babe7a21ed314f0fab2c0a09d3a05d6b
Frank Breedijk <fbreedijk@schubergphilis.com>
2017-12-08 22:47:49 +0100

    THere was an error in the spec file I made it when I converted tabs to spaces

--- rpm/makerpm.sh
+++ rpm/makerpm.sh
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-set -x
+#set -x
 set -e
 UPSTREAM_VERSION=$1
 COMMITS=$2
