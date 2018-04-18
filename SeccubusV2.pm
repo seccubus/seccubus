a4e3b5b5c809bba092d19bc0247194124b673913
Daniele Bonomi <dbonomi@schubergphilis.com>
2018-04-18 15:46:25 +0200

    Copyright

--- SeccubusV2.pm
+++ SeccubusV2.pm
@@ -1,5 +1,5 @@
 # ------------------------------------------------------------------------------
-# Copyright 2017 Frank Breedijk, Glenn ten Cate
+# Copyright 2017 Frank Breedijk, Glenn ten Cate, Daniele Bonomi
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
