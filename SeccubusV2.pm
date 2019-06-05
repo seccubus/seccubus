ba575a6a1fff55382a494016ad2ef4ebba5aa114
Alex Smirnoff <arkenoi@gmail.com>
2019-06-05 14:19:36 +0300

    Fixed copyright string

--- SeccubusV2.pm
+++ SeccubusV2.pm
@@ -1,5 +1,5 @@
 # ------------------------------------------------------------------------------
-# Copyright 2011-2019 Frank Breedijk, Glenn ten Cate, Daniele Bonomi
+# Copyright 2011-2019 Frank Breedijk, Glenn ten Cate, Daniele Bonomi, Alex Smirnoff
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
